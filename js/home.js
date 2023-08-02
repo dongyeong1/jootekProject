f6d0053ce43383783e1baa8ca36aa6615faf0b38
dongyeong1 <rjsdud0227@naver.com>
2023-08-03 00:10:29 +0900

    menu

--- js/home.js
+++ js/home.js
@@ -1,13 +1,28 @@
-let underline = document.querySelector(".underline");
-let menus = document.querySelectorAll(".headermenu a");
+// let dropmenu = document.querySelector(".dropMenu");
+// // let hovermeunu = document.querySelector(".hovermenu");
+// let menus = document.querySelectorAll(".headermenu");
+// menus.forEach((e) => {
+//     e.addEventListener("mouseover", (e) => horizontalIndicator(e));
+// });
+// // menus.forEach((e) => {
+// //     e.addEventListener("mouseout", (e) => out(e));
+// // });
+// dropmenu.forEach((e) => {
+//     e.addEventListener("mouseout", (e) => out(e));
+// });
 
-menus.forEach((e) => {
-    e.addEventListener("mouseover", (e) => horizontalIndicator(e));
-});
+// function horizontalIndicator(e) {
+//     dropmenu.style.opacity = 1;
+//     // dropmenu.style.display = "block";
 
-function horizontalIndicator(e) {
-    underline.style.left = e.currentTarget.offsetLeft - 55 + "px";
-    // underline.style.width = e.currentTarget.offsetWidth + "px";
-    underline.style.width = 20 + "%";
-    underline.style.top = 65 + "px";
-}
+//     // underline.style.left = e.currentTarget.offsetLeft - 55 + "px";
+//     // // underline.style.width = e.currentTarget.offsetWidth + "px";
+//     // underline.style.width = 20 + "%";
+//     // underline.style.top = 78 + "px";
+// }
+
+// function out(e) {
+//     dropmenu.style.opacity = 0;
+
+//     // dropmenu.style.display = "none";
+// }
